337c659b3436b7c221a6e423688efabde983cfef
Gabriel <garciasholding@gmail.com>
2022-07-11 11:26:57 -0300

    Practicing Dom & mini project "fruit list"

---
+++ dom/fruit list/script.js
@@ -0,0 +1,14 @@
+let products = ['Banana','Morango','Goiaba','Pêssego'];
+
+function createElementItens (arrayOfFruits) {
+    let ul = document.querySelector('.list-itens');
+    let fruitList = [];
+    
+    for(let i = 0; i < arrayOfFruits.length; i++) {
+        let fruitCard = document.createElement('li');
+        fruitCard.innerText = arrayOfFruits[i];
+        ul.appendChild(fruitCard);
+    }
+}
+
+createElementItens(products);
---
+++ dom/practicing/script.js
@@ -0,0 +1,10 @@
+let paragrafo = document.createElement('p');
+paragrafo.innerText = 'kkkasdasdasdaskkk'
+
+document.querySelector('body').appendChild(paragrafo);
+
+const tagInput = document.createElement('input');
+tagInput.placeholder = 'Digite algo'
+
+const form = document.querySelector('form');
+form.appendChild(tagInput)
